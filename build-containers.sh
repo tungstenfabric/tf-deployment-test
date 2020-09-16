4a467ddbafa06e73f12b1704b88cdb8e1014777e
Ivan Grebenschikov <van.grebenschikov@gmail.com>
2020-09-16 10:41:57 +0300

    Add build-containers.sh, Dockerfile and entrypoint.sh
    Change-Id: Iccaade97d66179702a7b4b6911d66cab377e73a5

---
+++ build-containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+scriptdir=$(realpath $(dirname "$0"))
+
+build_opts="--build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8"
+build_opts+=" --network host --no-cache --tag tf-deployment-test --tag ${CONTAINER_REGISTRY}/${CONTRAIL_CONTAINER_TAG} $scriptdir"
+
+sudo docker build $build_opts
