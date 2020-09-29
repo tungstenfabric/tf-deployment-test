efaef743424ad2e0f26c3770030ba31a19b8c8ba
Ivan Grebenschikov <van.grebenschikov@gmail.com>
2020-09-29 19:56:00 +0300

    Update entrypoint.sh and testrunner.sh with adding stack.env
    partial-jira-bug: CEM-19031
    
    Change-Id: I21bba495daa0d74d095eb9f2c7882ddf27cdebd2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source stack.env
 
 echo "tf-deployment-test/entrypoint.sh finished"
--- testrunner.sh
+++ testrunner.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+TF_CONFIG_DIR=${TF_CONFIG_DIR:-"${HOME}/.tf"}
 
 CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"localhost:5000"}
 CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"dev"}
 
-sudo docker run ${CONTAINER_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG}
+sudo docker run -v ${TF_CONFIG_DIR}/stack.env:/stack.env ${CONTAINER_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG}'
+
 
 echo tf-deployment-test/testrunner.sh finished
