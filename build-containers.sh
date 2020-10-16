92cb7c9f94c052a6da36620ce6fe4b75ff245c89
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-10-16 13:22:28 +0300

    Fix an error
    Change-Id: I8fe9572290c32b4c6d8bf74e8d7e96cffe398cfc
    partial-jira-bug: 19031

--- build-containers.sh
+++ build-containers.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"localhost:5000"}
+CONTRAIL_REGISTRY=${CONTRAIL_REGISTRY:-"localhost:5000"}
 CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"dev"}
 
 scriptdir=$(realpath $(dirname "$0"))
 
 build_opts="--build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8"
-build_opts+=" --network host --no-cache --tag ${CONTAINER_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG} $scriptdir"
+build_opts+=" --network host --no-cache --tag ${CONTRAIL_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG} $scriptdir"
 
 sudo docker build $build_opts
-if [[ -n "$CONTAINER_REGISTRY" ]]; then
-  sudo docker push ${CONTAINER_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG}
+if [[ -n "$CONTRAIL_REGISTRY" ]]; then
+  sudo docker push ${CONTRAIL_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG}
 fi
