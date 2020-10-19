9783ce657ec5380f5365941c3c71e00a5db219f5
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 11:27:29 +0000

    Deployment test infra partial-jira-bug: CEM-19031
    Change-Id: I88452cb061f6377bb480adff6aede6fbb97e5f16

---
+++ scripts/test_bash_jumphost.py
@@ -0,0 +1,14 @@
+from deployment_test import BaseTestCase
+from testtools.testcase import attr, WithAttributes
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+class BashJumphostTests(WithAttributes,BaseTestCase):
+    # k8s_manifests-kubernetes attr means deployer is k8s_manifests
+    # and orchestrator  is kubernetes
+    # this test will runs for k8s_manifests depplyer and kubernetes orchestrator
+    @attr("k8s_manifests-kubernetes")
+    def test_manifests_k8s_smoke(self):
+        seld.logger = logging.getLogger(__name__ + '.manifests_k8s_smoke')
+        self.run_bash_test_on_host('k8s_manifests_k8s.sh')
