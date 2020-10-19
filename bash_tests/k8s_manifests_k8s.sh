d26475ca90d6b8b2d951e5b3f99c37f13f427a4f
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 11:27:29 +0000

    Deployment test infra partial-jira-bug: CEM-19031
    Change-Id: I88452cb061f6377bb480adff6aede6fbb97e5f16

---
+++ bash_tests/k8s_manifests_k8s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# k8s manifests deployer, k8s orchestrator smoke test
+
+agent_pod_phase=$(kubectl get pods --all-namespaces -o custom-columns==NAME:.metadata.name,STATUS:.status.phase | grep "contrail-agent" | head -1)
+agent_pod=$(echo $agent_pod_phase | cut -d " " -f1)
+agent_phase=$(echo $agent_pod_phase | cut -d " " -f2)
+echo "Agent pod is: $agent_pod"
+if [[ "$agent_phase" != "Running" ]] ; then
+  echo "ERROR: agent pod $agent_pod is not in Running state" > /proc/self/fd/2
+fi
+exit 1
