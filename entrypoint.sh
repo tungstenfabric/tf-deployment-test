8ba533fb149a15e08a9d75cd96c779fdfa1d3c4e
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 11:27:29 +0000

    Deployment test infra
    Change-Id: Ib79879924965d9b8d37f0678d18ad09ed8135094

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -e
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
 
-source /root/.tf/stack.env
+if [[ -f /root/.tf/stack.env ]] ; then
+    source=/root/.tf/stack.env
+fi
 
-# TODO: run tests
+export ORCHESTRATOR=${ORCHESTRATOR:=kubernetes}
+export WORKSPACE=$my_dir
+source /env/bin/activate
+cd $WORKSPACE
+if [[ ! -d "$WORKSPACE/.testrepository" ]]; then
+    testr init
+fi
 
-# TODO: save logs
-
-# TODO: check result and print some message
-echo "INFO: tests finished"
+tests_tag="${ORCHESTRATOR}_and_${DEPLOYER}"
+testr run --subunit ${tests_tag}
+    
