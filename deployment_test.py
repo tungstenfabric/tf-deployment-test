e6f222324e386dae4a81ce4bd15a0747b33fd5ef
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 11:27:29 +0000

    Deployment test infra partial-jira-bug: CEM-19031
    Change-Id: I88452cb061f6377bb480adff6aede6fbb97e5f16

---
+++ deployment_test.py
@@ -0,0 +1,23 @@
+import testtools
+from my_fixtures import HostFixture
+import os
+
+class BaseTestCase(testtools.TestCase):
+    def setUp(self):
+        self.host_fixture = self.useFixture(HostFixture())
+        self.workspace_path = os.getenv('WORKSPACE')
+        self.host_fixture.copy_local_file_to_remote(self.workspace_path,"/tmp/tf-deploymant-test")
+        super(BaseTestCase, self).setUp()
+
+    def check_cmd_on_host(self,cmd):
+        (exec_res, stdout, stderr) = self.host_fixture.exec_on_host(cmd)
+        for line in stdout.readlines():
+            self.logger.info("%s " % line)
+        for line in stderr.readlines():
+            self.logger.info("bash.stderr: %s " % line)
+        self.assertFalse(exec_res)
+
+    def run_bash_test_on_host(
+            self, bash_file_name):
+        remote_path = os.path.join("/tmp/tf-deployment-test", bash_file_name)
+        self.check_cmd_on_host(remote_path)
---
+++ my_fixtures/host_fixture.py
@@ -0,0 +1,47 @@
+import fixtures
+import paramiko
+from paramiko.ssh_exception import SSHException
+import os
+import sys
+from testtools.content import text_content,attach_file
+import logging
+
+class HostFixture(fixtures.Fixture):
+    def _setUp(self):
+        self.host_user = os.getenv("TF_HOST_USER")
+        self.host_key = os.getenv("TF_SSH_KEY")
+        self.host = os.getenv("TF_HOST_ADDR")
+        with open(os.path.expanduser('pk.key'), 'w') as fd:
+            fd.write(self.host_key)
+        if( not self.host_user or not self.host_key or not self.host):
+            raise Exception("ERROR: Need to pass host credentials to run the tests")
+        self._client = paramiko.SSHClient()
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self._client.connect(self.host, username = self.host_user,
+                key_filename = os.path.expanduser('pk.key'), timeout = 5)
+        self.addCleanup(delattr, self, 'host_user')
+        self.addCleanup(delattr, self, 'host_key')
+        self.addCleanup(delattr, self, 'host')
+        self.addCleanup(self._client.close)
+
+    def exec_on_host(self, command):
+        if(not self._client):
+            raise Exception("ERROR: connection has not been set up yet")
+        exec_res = 0
+        chan = self._client.get_transport().open_session()
+        stdout = chan.makefile()
+        stderr = chan.makefile_stderr()
+        try:
+            chan.exec_command(command)
+            exec_res = chan.recv_exit_status()
+        except SSHException:
+            exec_res = 1
+        return (exec_res, stdout, stderr)
+
+    def copy_local_file_to_remote(self,localPath, remotePath):
+        if(not self._client):
+            raise Exception("ERROR: connection has not been set up yet")
+        with self._client.open_sftp() as ftp_client:
+            ftp_client.put(localPath, remotePath)
+
+
