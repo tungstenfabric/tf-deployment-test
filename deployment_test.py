2dd4afbbef98c0d0ceb9598994b109defc91a6a8
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 11:27:29 +0000

    Deployment test infra partial-jira-bug: CEM-19031
    Change-Id: I88452cb061f6377bb480adff6aede6fbb97e5f16

---
+++ deployment_test.py
@@ -0,0 +1,21 @@
+import testtools
+from my_fixtures import HostFixture
+import os
+
+class BaseTestCase(testtools.TestCase):
+    def setUp(self):
+        self.host_fixture = self.useFixture(HostFixture())
+        self.workspace_path = os.getenv('WORKSPACE')
+        super(BaseTestCase, self).setUp()
+
+    def check_cmd_on_host(self,cmd):
+        (exec_res, stdout, stderr) = self.host_fixture.exec_on_host(cmd)
+        for line in stdout.readlines():
+            self.logger.info("%s " % line)
+        for line in stderr.readlines():
+            self.logger.info("bash.stderr: %s " % line)
+        self.assertFalse(exec_res)
+
+    def run_bash_test_on_host(self, bash_file_name):
+        remote_path = os.path.join("/tmp/tf-deployment-test", bash_file_name)
+        self.check_cmd_on_host(remote_path)
