c6845c179a4e7d03b136993ed665387093fd7b69
Ivan TImofeev <ivanv.timofeev@gmail.com>
2020-10-19 11:27:29 +0000

    Deployment test infra partial-jira-bug: CEM-19031
    Change-Id: I88452cb061f6377bb480adff6aede6fbb97e5f16

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -e
-
-source /root/.tf/stack.env
-
-# TODO: run tests
-
-# TODO: save logs
-
-# TODO: check result and print some message
-echo "INFO: tests finished"
+set -exo pipefail
+if [[ -f /root/.tf/stack.env ]] ; then
+    set -a
+    source /root/.tf/stack.env
+    set +a
+fi
+if [[ -z "$ORCHESTRATOR" || -z "$DEPLOYER" ]]; then
+    echo "ERROR: ORCHESTRATOR and DEPLOYER must be set in stack.env"
+    exit 1
+fi
+export WORKSPACE=/tf-deployment-test
+cd $WORKSPACE
+if [[ ! -d "$WORKSPACE/.testrepository" ]]; then
+    testr init
+fi
+tests_tag="${DEPLOYER}-${ORCHESTRATOR}"
+tail -f /dev/null
+testr run --subunit ${tests_tag} | subunit2junitxml -f -o report.xml
