0de9dac0cb0226a6eef729fa894b9b55743eb93c
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-10-21 13:06:16 +0300

    test apply_defaults for Ansible deployment (WIP) v1.02
    Change-Id: Ifb4cc814ec08572aea766a25f5200340072caaff

---
+++ apply_defaults_tests/env_profile.sh
@@ -0,0 +1,10 @@
+# ----------------- from repo: tf-dev-test
+TF_CONFIG_DIR=${TF_CONFIG_DIR:-"${HOME}/.tf"}
+TF_STACK_PROFILE="${TF_CONFIG_DIR}/stack.env"
+# -----------------
+if [ -e "$TF_STACK_PROFILE" ] ; then
+  cat "$TF_STACK_PROFILE"
+  source "$TF_STACK_PROFILE"
+fi
+# -----------------
+export SSH_OPTIONS=${SSH_OPTIONS:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
