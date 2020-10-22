c674780d9f1018e6e0f0515c205303790d0d7dd1
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-10-22 13:18:29 +0300

    test apply_defaults for Ansible deployment (WIP) v1.09
    Change-Id: Ifb4cc814ec08572aea766a25f5200340072caaff

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
 FROM centos:7
 COPY ./*.sh /
+
+COPY requirements.txt .
+# install most packages from pypi and only then python-Fabric
+RUN yum install -y sudo python3 && \
+    pip3 install --no-cache-dir -r requirements.txt
+
+COPY apply_defaults_tests /apply_defaults_tests/
+
+RUN echo "ls -la /apply_defaults_tests/*" \
+    ls -la /apply_defaults_tests/*
+
+# CMD ["/usr/bin/tail","-f","/dev/null"]
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -e
+set -x
 
 source /root/.tf/stack.env
 
+echo "pwd is $(pwd)"
+ls -la
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+echo "my_file is $my_file and my_dir is $my_dir (expected apply_defaults_tests)"
+
 # TODO: run tests
+echo "ls -la"
+ls -la
+cat /apply_defaults_tests/test_apply_defaults_true.sh
+sudo bash /apply_defaults_tests/test_apply_defaults_true.sh
 
 # TODO: save logs
+
 
 # TODO: check result and print some message
 echo "INFO: tests finished"
