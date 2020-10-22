0bd2c5a390d817c8f303985b7c36580ff8060e75
Gleb Galkin <gleb108@gmail.com>
2020-10-22 19:47:34 +0300

    Using local mirrors for CICD
    Change-Id: I6dd84f44118b71cc0447970e3769781c6bc76ef1

--- rhosp/ffu_ziu_13_16/run.sh
+++ rhosp/ffu_ziu_13_16/run.sh
@@ -22,27 +22,27 @@ echo "Copiyng ffu/* to undercloud node"
 scp -r ./ffu ./rhosp-environment.sh $SSH_USER@$mgmt_ip:
 
 echo "Preparing for undercloud RHEL upgrade"
-run_ssh_undercloud './ffu/01_undercloud_prepare.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/01_undercloud_prepare.sh'
 reboot_and_wait_undercloud
 
-run_ssh_undercloud './ffu/02_undercloud_upgrade_rhel_step1.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/02_undercloud_upgrade_rhel_step1.sh'
 reboot_and_wait_undercloud
 
-run_ssh_undercloud './ffu/03_undercloud_upgrade_rhel_step2.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/03_undercloud_upgrade_rhel_step2.sh'
 reboot_and_wait_undercloud
 
-run_ssh_undercloud './ffu/04_undercloud_upgrade_tripleo.sh'
-run_ssh_undercloud './ffu/05_contrail_images_prepare.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/04_undercloud_upgrade_tripleo.sh'
+run_ssh_undercloud './ffu/tf_specific/05_contrail_images_prepare.sh'
 
 ######################################################
 #                  OVERCLOUD                         #
 ######################################################
-run_ssh_undercloud './ffu/06_overcloud_prepare.sh'
-run_ssh_undercloud './ffu/07_overcloud_prepare_templates.sh'
-run_ssh_undercloud './ffu/08_overcloud_upgrade_prepare.sh'
-run_ssh_undercloud './ffu/09_overcloud_upgrade_os.sh'
-run_ssh_undercloud './ffu/10_overcloud_upgrade_contrail_ctrl.sh'
-run_ssh_undercloud './ffu/11_overcloud_upgrade_compute.sh'
-run_ssh_undercloud './ffu/12_overcloud_upgrade_converge.sh'
-run_ssh_undercloud './ffu/13_collect_information.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/06_overcloud_prepare.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/07_overcloud_prepare_templates.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/08_overcloud_upgrade_prepare.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/09_overcloud_upgrade_os.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/10_overcloud_upgrade_contrail_ctrl.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/11_overcloud_upgrade_compute.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/12_overcloud_upgrade_converge.sh'
+run_ssh_undercloud './ffu/redhat_ffu_steps/13_collect_information.sh'
 
