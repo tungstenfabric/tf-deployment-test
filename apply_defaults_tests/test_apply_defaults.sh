6e8dcf88d3d85d28e257d6504194653655c21f05
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-10-26 18:57:47 +0300

    test apply_defaults for Ansible deployment (WIP) v1.65
    partial-jira-bug: CEM-19884
    Change-Id: Ifb4cc814ec08572aea766a25f5200340072caaff

---
+++ apply_defaults_tests/test_apply_defaults.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -ex
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source /root/.tf/stack.env
+
+export CONTROLLER_NODES="${CONTROLLER_NODES}"
+export SSH_OPTIONS=${SSH_OPTIONS:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
+export SSH_USER=${SSH_USER:-"centos"}
+
+IFS=', ' read -r -a array_controller_nodes <<< "$CONTROLLER_NODES"
+node=${array_controller_nodes[0]}
+echo "node is $node"
+
+# get APPLY_DEFAULTS value
+scp $SSH_OPTIONS "centos@$node:~/tf-ansible-deployer/instances.yaml" / 2>/dev/null
+instances_yaml_file='/instances.yaml'
+echo "cat $instances_yaml_file"
+cat $instances_yaml_file
+apply_defaults_value=$(python3 $my_dir/tools/get_apply_default.py $instances_yaml_file)
+echo "apply_defaults is $apply_defaults_value"
+
+encap_before_test=$(python3 $my_dir/tools/get_encap_priority.py $node)
+echo "encap_before_test is $encap_before_test"
+
+default_encap_priority='MPLSoUDP,MPLSoGRE,VXLAN'
+if [[ "$encap_before_test" != "$default_encap_priority" ]] ; then
+  echo "ERROR: current encap_priority is not default $default_encap_priority"
+  exit 1
+fi
+
+new_encap_priority='VXLAN,MPLSoUDP,MPLSoGRE'
+echo "we begining set encap_priority = $new_encap_priority"
+$(python3 $my_dir/tools/set_encap_priority.py $node $new_encap_priority)
+
+encap_after_change=$(python3 $my_dir/tools/get_encap_priority.py $node)
+echo "encap_after_change is $encap_after_change"
+
+if [[ "$encap_before_test" == "$encap_after_change" ]] ; then
+  echo "ERROR: encap_priority was not changed by api"
+  exit 1
+fi
+
+# TODO: reboot only contrail containers
+for machine in ${array_controller_nodes[*]} ; do
+  ssh $SSH_OPTIONS "centos@$machine" "sudo docker restart \$(sudo docker ps -q | grep -v ^$HOSTNAME) &>/dev/null" 2>/dev/null &
+done
+wait
+sleep 60
+
+encap_after_restart=$(python3 $my_dir/tools/get_encap_priority.py $node)
+echo "encap_after_restart is $encap_after_restart"
+
+# final check
+if [[ "$apply_defaults_value" == true ]] ; then
+  if [[ "$default_encap_priority" != "$encap_after_restart" ]] ; then
+    echo "ERROR: encap_priority was not reseted after restarting containers"
+    exit 1
+  fi
+else
+  if [[ "$encap_before_test" == "$encap_after_restart" ]] ; then
+    echo "ERROR: encap_priority was reseted after restarting containers"
+    exit 1
+  fi
+fi
+
+echo "$my_file: PASSED"
+
+exit 0
---
+++ apply_defaults_tests/tools/get_encap_priority.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+from vnc_api import vnc_api
+
+# TODO: add supproting SSL
+my_hostname = sys.argv[1]
+# my_fq_name type list
+my_fq_name = ['default-global-system-config', 'default-global-vrouter-config']
+vnc_lib = vnc_api.VncApi(api_server_host=my_hostname)
+gr_obj = vnc_lib.global_vrouter_config_read(fq_name=my_fq_name)
+encap_priority = gr_obj.get_encapsulation_priorities()
+print(str(encap_priority)[17:-1].replace("'", "").replace(" ", ""))
+# before "encapsulation = ['MPLSoUDP', 'MPLSoGRE', 'VXLAN']"
+# after "MPLSoUDP,MPLSoGRE,VXLAN"
