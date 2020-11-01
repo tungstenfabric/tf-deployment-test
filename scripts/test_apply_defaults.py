5a8ce9bd327d568211d8b2fbed0a65a6adf0d753
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-11-02 00:51:43 +0300

    test apply_defaults for Ansible deployment (WIP) v3.25
    step 05 full complete: refactoring
    
    partial-jira-bug: CEM-19884
    Change-Id: I6e8b20ea42ec5d682d189a85c17f9f654c09ab42

---
+++ scripts/test_apply_defaults.py
@@ -0,0 +1,41 @@
+from deployment_test import BaseTestCase
+from testtools.testcase import attr, WithAttributes
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+
+class BashApplyDefaultsTests(WithAttributes,BaseTestCase):
+    # ansible-kubernetes attr means deployer is k8s_manifests
+    # and orchestrator  is kubernetes
+    # this test will runs for k8s_manifests depplyer and kubernetes orchestrator
+    @attr("ansible-kubernetes")
+    def test_ansible_k8s_smoke(self):
+        self.logger = logging.getLogger(__name__ + '.ansible_k8s_smoke')
+        # -------------
+        self.add_to_log('Go test check')
+        # -------------
+        master_node = self.get_master_node()
+        # -------------
+        encap_before_test = self.get_encap_priority(master_node)
+        default_encap_priority = 'MPLSoUDP,MPLSoGRE,VXLAN'
+        self.assert_encap_before_default(encap_before_test, default_encap_priority)
+        # -------------
+        new_encap_priority = 'VXLAN,MPLSoUDP,MPLSoGRE'
+        self.set_encap_priority(new_encap_priority, master_node)
+        encap_after_change = self.get_encap_priority(master_node)
+        self.assert_encap_before_after_change(encap_before_test, encap_after_change)
+        # -------------
+        cont_name = "tf-deployment-test"
+        self.reboot_containers_without_our_container_by_name(cont_name)
+        # -------------
+        encap_after_restart = self.get_encap_priority(master_node)
+        # -------------
+        apply_defaults = self.get_apply_defaults_value_from_env()
+        if apply_defaults == "true":
+            self.assert_encap_default_after_restart(default_encap_priority, encap_after_restart)
+        else:
+            self.assert_encap_before_after_restart(encap_after_change, encap_after_restart)
+        # ------------
+        self.add_to_log('apply_default test: PASSED')
