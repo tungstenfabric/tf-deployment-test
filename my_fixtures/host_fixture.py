908a5a4247576fd1d5741b4222f6085f72bc1093
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-11-03 19:35:07 +0300

    test apply_defaults for Ansible deployment (WIP) v4.10
    step 05: full complete
    
    partial-jira-bug: CEM-19884
    Change-Id: I6e8b20ea42ec5d682d189a85c17f9f654c09ab42

--- my_fixtures/host_fixture.py
+++ my_fixtures/host_fixture.py
@@ -4,37 +4,38 @@ from paramiko.ssh_exception import SSHException
 import os
 import sys
 import stat
-from testtools.content import text_content,attach_file
+from testtools.content import text_content, attach_file
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
+
 class HostFixture(fixtures.Fixture):
-    def _setUp(self):
-        self.host_user = os.getenv("TF_HOST_USER")
-        self.host_key = os.getenv("TF_SSH_KEY")
-        self.host = os.getenv("TF_HOST_ADDR")
+
+    def _setUp(self, hostname=None, username=None, key_filename=None, timeout=5):
+        self.logger = logging.getLogger(__name__ + '.HostFixture')
+        host_user = username or os.getenv("TF_HOST_USER")
+        host_key = key_filename or os.getenv("TF_SSH_KEY")
+        self.host = hostname or os.getenv("TF_HOST_ADDR")
+        self.logger.info(f"init ssh connection with {self.host}")
         ssh_file = os.path.expanduser('pk.key')
         with open(ssh_file, 'w') as fd:
-            fd.write(self.host_key)
+            fd.write(host_key)
         os.chmod(ssh_file, stat.S_IRWXU)
-        rsync_cmd = "rsync -Pav -e \"ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\" /tf-deployment-test %s@%s:/tmp/" % (ssh_file, self.host_user, self.host)
+        rsync_cmd = "rsync -Pav -e \"ssh -i %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\" /tf-deployment-test %s@%s:/tmp/" % (
+        ssh_file, host_user, self.host)
         code = os.system(rsync_cmd)
-        if(code):
+        if code:
             raise Exception("ERROR: rsync tests to host fails with code %s " % code)
-        if( not self.host_user or not self.host_key or not self.host):
+        if not host_user or not host_key or not self.host:
             raise Exception("ERROR: Need to pass host credentials to run the tests")
         self._client = paramiko.SSHClient()
         self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self._client.connect(self.host, username = self.host_user,
-                key_filename = ssh_file, timeout = 5)
-        self.addCleanup(delattr, self, 'host_user')
-        self.addCleanup(delattr, self, 'host_key')
-        self.addCleanup(delattr, self, 'host')
-        self.addCleanup(self._client.close)
+        self._client.connect(hostname=self.host, username=host_user, key_filename=ssh_file, timeout=timeout)
 
     def exec_on_host(self, command):
-        if(not self._client):
+        if not self._client:
             raise Exception("ERROR: connection has not been set up yet")
-        exec_res = 0
         chan = self._client.get_transport().open_session()
         stdout = chan.makefile()
         stderr = chan.makefile_stderr()
@@ -45,10 +46,23 @@ class HostFixture(fixtures.Fixture):
             exec_res = 1
         return (exec_res, stdout, stderr)
 
+    def check_cmd_on_node_by_ssh(self, cmd):
+        (exec_res, stdout, stderr) = self.exec_on_host(cmd)
+        output_text = ""
+        self.logger.info("-" * 20)
+        self.logger.info(f"vipolnim na {self.host} po ssh: {cmd}")
+        for line in stdout.readlines():
+            # self.logger.info("%s " % line)
+            output_text += f"{line}\n"
+        for line in stderr.readlines():
+            self.logger.info("bash.stderr: %s " % line)
+        self.logger.info(f"exec_res: {exec_res}")
+        self.logger.info(f"return ssh:\n{output_text}".strip())
+        self.logger.info("=" * 20)
+        return output_text
+
     def copy_local_file_to_remote(self,localPath, remotePath):
         if(not self._client):
             raise Exception("ERROR: connection has not been set up yet")
         with self._client.open_sftp() as ftp_client:
             ftp_client.put(localPath, remotePath)
-
-
