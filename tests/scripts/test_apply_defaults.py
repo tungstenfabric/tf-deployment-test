5e5ffc5dd8933565a560ac16f6ca7afa6063b2aa
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-11-04 23:59:13 +0300

    test apply_defaults for Ansible deployment (WIP) v4.11
    step 05: full complete
    
    partial-jira-bug: CEM-19884
    Change-Id: I6e8b20ea42ec5d682d189a85c17f9f654c09ab42

---
+++ tests/scripts/test_apply_defaults.py
@@ -0,0 +1,40 @@
+from my_fixtures.vnc_api_fixture import VncApiFixture
+from my_fixtures.host_fixture import HostFixture
+from testtools.testcase import attr, WithAttributes
+from common.deployment_test import BaseTestCase
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+
+
+class ApplyDefaultsTests(WithAttributes, BaseTestCase):
+
+    @attr("all-deployers-all-orchestrator")
+    def test_apply_defaults(self):
+        self.logger = logging.getLogger(__name__ + '.test_apply_defaults')
+        self.logger.info('Go test check')
+        vnc_api_client = self.useFixture(VncApiFixture())
+
+        encap_before_test = vnc_api_client.get_encap_priority()
+        default_encap_priority = 'MPLSoUDP,MPLSoGRE,VXLAN'
+        assert encap_before_test == default_encap_priority, f"ERROR: current encap_priority is not default {default_encap_priority}"
+
+        new_encap_priority = 'VXLAN,MPLSoUDP,MPLSoGRE'
+        vnc_api_client.set_encap_priority(new_encap_priority)
+        encap_after_change = vnc_api_client.get_encap_priority()
+        assert encap_before_test != encap_after_change, "ERROR: encap_priority was not changed by api"
+
+        cont_name = "tf-deployment-test"
+        self.restart_containers_without_our_container_by_name(cont_name)
+
+        encap_after_restart = vnc_api_client.get_encap_priority()
+
+        apply_defaults = os.getenv("APPLY_DEFAULTS", True)
+        self.logger.info(f'apply_defaults is {apply_defaults}')
+        if bool(apply_defaults):
+            assert default_encap_priority == encap_after_restart, "ERROR: encap_priority was not reseted after restarting containers"
+        else:
+            assert encap_after_change == encap_after_restart, "ERROR: encap_priority was reseted after restarting containers"
+
+        self.logger.info('apply_default test: PASSEDs')
