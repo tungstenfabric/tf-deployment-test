22683e71a44926250d9496ee77a681bb46d6c037
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-11-04 23:59:13 +0300

    test apply_defaults for Ansible deployment (WIP) v4.11
    step 05: full complete
    
    partial-jira-bug: CEM-19884
    Change-Id: I6e8b20ea42ec5d682d189a85c17f9f654c09ab42

--- my_fixtures/host_fixture.py
+++ my_fixtures/host_fixture.py
@@ -4,14 +4,22 @@ from paramiko.ssh_exception import SSHException
 import os
 import sys
 import stat
-from testtools.content import text_content,attach_file
+from testtools.content import text_content, attach_file
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
+
 class HostFixture(fixtures.Fixture):
+
+    def __init__(self, hostname=None, username=None):
+        self.logger = logging.getLogger(__name__ + '.HostFixture')
+        self.host_user = username or os.getenv("TF_HOST_USER")
+        self.host = hostname or os.getenv("TF_HOST_ADDR")
+        self.logger.info(f"init ssh connection with {self.host}")
+
     def _setUp(self):
-        self.host_user = os.getenv("TF_HOST_USER")
         self.host_key = os.getenv("TF_SSH_KEY")
-        self.host = os.getenv("TF_HOST_ADDR")
         ssh_file = os.path.expanduser('pk.key')
         with open(ssh_file, 'w') as fd:
             fd.write(self.host_key)
@@ -32,9 +40,8 @@ class HostFixture(fixtures.Fixture):
         self.addCleanup(self._client.close)
 
     def exec_on_host(self, command):
-        if(not self._client):
+        if not self._client:
             raise Exception("ERROR: connection has not been set up yet")
-        exec_res = 0
         chan = self._client.get_transport().open_session()
         stdout = chan.makefile()
         stderr = chan.makefile_stderr()
@@ -45,10 +52,23 @@ class HostFixture(fixtures.Fixture):
             exec_res = 1
         return (exec_res, stdout, stderr)
 
+    def check_cmd_on_node_by_ssh(self, cmd):
+        (exec_res, stdout, stderr) = self.exec_on_host(cmd)
+        output_text = ""
+        self.logger.info("-" * 20)
+        self.logger.info(f"vipolnim na {self.host} po ssh: {cmd}")
+        for line in stdout.readlines():
+            # self.logger.info("%s " % line)
+            output_text += f"{line}\n"
+        for line in stderr.readlines():
+            self.logger.info("bash.stderr: %s " % line)
+        self.logger.info(f"exec_res: {exec_res}")
+        self.logger.info(f"return ssh:\n{output_text}".strip())
+        self.logger.info("=" * 20)
+        return output_text
+
     def copy_local_file_to_remote(self,localPath, remotePath):
-        if(not self._client):
+        if not self._client:
             raise Exception("ERROR: connection has not been set up yet")
         with self._client.open_sftp() as ftp_client:
             ftp_client.put(localPath, remotePath)
-
-
