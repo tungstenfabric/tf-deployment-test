badf42411ea41f172116f7dcbfdfd333b2cfb95a
Gleb Galkin <gleb108@gmail.com>
2020-11-05 17:40:14 +0300

    Minor fixes
    parallel ssh running for yum update
    rhel8.repo templated
    new variable RHEL_LOCAL_MIRROR_FFU
    fixing ipmi driver
    retrying some command
    redhat's playbook-nics.yaml refreshed
    fixing Red Hat doc numeration
    
    Change-Id: Id3493d0ad23bda173d9ae30830f0b638fc5cb8c0

--- rhosp/ffu_ziu_13_16/functions.sh
+++ rhosp/ffu_ziu_13_16/functions.sh
@@ -85,3 +85,19 @@ function add_variable() {
   echo "export $var_name=\"$var_value\"" >>$file
 }
 
+function retry() {
+    local -r -i max_attempts="$1"; shift
+    local -i attempt_num=1
+    until "$@"
+    do
+        if ((attempt_num==max_attempts))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+            sleep $((attempt_num++))
+        fi
+    done
+}
+ 
