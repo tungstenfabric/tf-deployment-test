a68b754aefeb1313fc2efd1cd8b808258cf6e5b3
Andrey Pavlov <andrey-mp@yandex.ru>
2020-11-21 14:21:55 +0300

    rework infra, rework apply_defaults test
    Change-Id: Ic9e96e7df6762019e35329b2973569bb0eae1331

---
+++ common/utils/vnc_api.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from vnc_api.vnc_api import EncapsulationPrioritiesType
+from vnc_api import vnc_api
+
+
+class VncApiProxy(object):
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__ + '.VncApiPropxy')
+        # list can be space or comma separated
+        controller_nodes = os.environ["CONTROLLER_NODES"].replace(",", " ").split(",")
+        # TODO: add keystone creds
+        # TODO: add ssl
+        self.vnc_lib = vnc_api.VncApi(api_server_host=controller_nodes)
+
+    def get_encap_priorities(self):
+        fq_name = ['default-global-system-config', 'default-global-vrouter-config']
+        gr_obj = self.vnc_lib.global_vrouter_config_read(fq_name=fq_name)
+        encap_obj = gr_obj.get_encapsulation_priorities()
+        if not encap_obj:
+            return ['', '', '']
+        encaps = encap_obj.encapsulation
+        encaps.extend([''] * (3 - len(encaps)))
+        return encaps
+
+    def set_encap_priority(self, encaps):
+        fq_name = ['default-global-system-config', 'default-global-vrouter-config']
+        encap_obj = EncapsulationPrioritiesType(encapsulation=encaps)
+        gr_obj = self.vnc_lib.global_vrouter_config_read(fq_name=fq_name)
+        gr_obj.set_encapsulation_priorities(encap_obj)
+        self.vnc_lib.global_vrouter_config_update(gr_obj)
