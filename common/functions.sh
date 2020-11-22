7b76dd76090218e000f7365a832024685bffc7ff
tikitavi <rtikitavi@gmail.com>
2020-11-23 00:58:09 +0300

    add deployment test for ziu in ansible
    Partal-jira-bug: CEM-19742
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: Idcce15544c64b7abd3cf544a0e0fca345e37d0ca

---
+++ common/functions.sh
@@ -0,0 +1,42 @@
+function check_tf_active() {
+  local machine
+  local line=
+  # TODO: where?
+  export SSH_OPTIONS=${SSH_OPTIONS:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
+
+  for machine in $(echo "$CONTROLLER_NODES $AGENT_NODES" | tr " " "\n" | sort -u) ; do
+    if ! ssh $SSH_OPTIONS $machine "command -v contrail-status" 2>/dev/null ; then
+      return 1
+    fi
+    for line in $(ssh $SSH_OPTIONS $machine "sudo contrail-status" 2>/dev/null | egrep ": " | grep -v "WARNING" | awk '{print $2}'); do
+      if [ "$line" != "active" ] && [ "$line" != "backup" ] ; then
+        return 1
+      fi
+    done
+  done
+  return 0
+}
+
+
+# pull deployer src container locally and extract files to path
+# Functions get two required params:
+#  - deployer image
+#  - directory path deployer have to be extracted to
+function fetch_deployer() {
+  if [[ $# != 2 ]] ; then
+    echo "ERROR: Deployer image name and path to deployer directory are required for fetch_deployer"
+    return 1
+  fi
+
+  local deployer_image=$1
+  local deployer_dir=$2
+
+  sudo rm -rf $deployer_dir
+
+  local image="$CONTAINER_REGISTRY/$deployer_image"
+  [ -n "$CONTRAIL_CONTAINER_TAG" ] && image+=":$CONTRAIL_CONTAINER_TAG"
+  sudo docker create --name $deployer_image --entrypoint /bin/true $image || return 1
+  sudo docker cp $deployer_image:/src $deployer_dir
+  sudo docker rm -fv $deployer_image
+  sudo chown -R $UID $deployer_dir
+}
