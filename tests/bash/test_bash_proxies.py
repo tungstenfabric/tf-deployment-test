ba6eb494e38acb2967262dc1b8fa1b7c167515e0
tikitavi <rtikitavi@gmail.com>
2020-11-23 00:58:09 +0300

    add deployment test for ziu in ansible
    Partal-jira-bug: CEM-19742
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: Idcce15544c64b7abd3cf544a0e0fca345e37d0ca
    Depends-on: Ie17c406ef2e4e1539d44ab8607ffdcabf434aee6

--- tests/bash/test_bash_proxies.py
+++ tests/bash/test_bash_proxies.py
@@ -16,13 +16,13 @@ class BashTests(WithAttributes, DeploymentTestCase):
     # k8s_manifests-kubernetes attr means deployer is k8s_manifests
     # and orchestrator  is kubernetes
     # this test will be run for k8s_manifests deployer and kubernetes orchestrator
-    @attr("k8s_manifests-kubernetes")
+    @attr("k8s_manifests", "kubernetes")
     def test_manifests_k8s_smoke(self):
         self.logger = logging.getLogger(__name__ + '.manifests_k8s_smoke')
         file_name = os.path.join(CURRENT_DIRECTORY, 'k8s_manifests_k8s.sh')
         self.run_test_remotely(file_name)
 
-    @attr("juju-all")
+    @attr("juju", "hybrid")
     def test_k8s_auth_keystone(self):
         self.logger = logging.getLogger(__name__ + '.k8s_auth_keystone')
         file_name = os.path.join(CURRENT_DIRECTORY, 'k8s_auth_keystone.sh')
