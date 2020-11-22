824525b27fb8bba32b115c03fbb919cc0ee3177f
tikitavi <rtikitavi@gmail.com>
2020-11-23 00:58:09 +0300

    add deployment test for ziu in ansible
    Partal-jira-bug: CEM-19742
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: Idcce15544c64b7abd3cf544a0e0fca345e37d0ca

--- tests/bash/test_bash_proxies.py
+++ tests/bash/test_bash_proxies.py
@@ -27,3 +27,9 @@ class BashTests(WithAttributes, DeploymentTestCase):
         self.logger = logging.getLogger(__name__ + '.k8s_auth_keystone')
         file_name = os.path.join(CURRENT_DIRECTORY, 'k8s_auth_keystone.sh')
         self.run_test_remotely(file_name)
+
+    @attr("ansible-openstack")
+    def test_ansible_ziu(self):
+        self.logger = logging.getLogger(__name__ + '.ansible_ziu')
+        file_name = os.path.join(CURRENT_DIRECTORY, '../ziu/ansible/ansible_ziu.sh')
+        self.run_test_remotely(file_name)
