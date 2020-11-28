2d60017c90a91ecd2411bf8da2c4d421ffcc60e0
Andrey Pavlov <andrey-mp@yandex.ru>
2020-11-28 14:25:51 +0300

    pass test.env to tests and copy it remotely
    Change-Id: I6d31c0e6eaddaa91cc4594b09e29fa2e8ee31c23

--- testrunner.sh
+++ testrunner.sh
@@ -5,15 +5,23 @@ scriptdir=$(realpath $(dirname "$0"))
 TF_CONFIG_DIR=${TF_CONFIG_DIR:-"${HOME}/.tf"}
 CONTAINER_REGISTRY=${CONTAINER_REGISTRY:-"localhost:5000"}
 CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"dev"}
+TEST_ENV_FILE=$TF_CONFIG_DIR/test.env
 
 TF_DEPLOYMENT_TEST_IMAGE="${TF_DEPLOYMENT_TEST_IMAGE:-${CONTAINER_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG}}"
 
-env_opts=""
-env_opts+=" --env SSH_USER=$(whoami)"
-env_opts+=" --env SSH_HOST=$(hostname -i | awk '{print $1}')"
+echo "INFO: create test.env"
+rm -f $TEST_ENV_FILE
+touch $TEST_ENV_FILE
+if [ -f $TF_CONFIG_DIR/stack.env ]; then
+  cat $TF_CONFIG_DIR/stack.env > $TEST_ENV_FILE
+fi
+echo "CONTAINER_REGISTRY_ORIGINAL=$CONTAINER_REGISTRY_ORIGINAL" >> $TEST_ENV_FILE
+echo "CONTRAIL_CONTAINER_TAG_ORIGINAL=$CONTRAIL_CONTAINER_TAG_ORIGINAL" >> $TEST_ENV_FILE
+echo "SSH_USER=$(whoami)" >> $TEST_ENV_FILE
+echo "SSH_HOST=$(hostname -i | awk '{print $1}')" >> $TEST_ENV_FILE
+cat $TEST_ENV_FILE
 
-vol_opts=""
-vol_opts+=" -v ${TF_CONFIG_DIR}:/root/.tf"
+vol_opts=" -v $TEST_ENV_FILE:/input/test.env"
 vol_opts+=" -v $HOME/.ssh/id_rsa:/root/.ssh/id_rsa"
 
 # NOTE: to be able to have sources locally that will be executed
@@ -22,7 +30,7 @@ if [ -d $scriptdir/tf-deployment-test ]; then
   vol_opts+=" -v $scriptdir/tf-deployment-test:/tf-deployment-test"
 fi
 
-sudo docker run --rm -i $vol_opts $env_opts $TF_DEPLOYMENT_TEST_IMAGE || res=1
+sudo docker run --rm -i $vol_opts --env-file $TEST_ENV_FILE $TF_DEPLOYMENT_TEST_IMAGE || res=1
 
 # TODO: collect logs
 
