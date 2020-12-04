9b5125bda102391f3983f1c71db6ed6968364759
Arina Argentova <aargentova@progmaticlab.com>
2020-12-04 18:33:09 +0300

    [+]  deployment test for ziu in rhosp13 rhosp16
    partial-jira-bug: CEM-20714
    partial-jira-bug: CEM-20715
    
    Depends-On: I609ba9e2fa11d29aa63b1a9fca3f241ded299d63
    Depends-On: I871a4bd1440c9e608eff3096030b3f7edb7f45cf
    Change-Id: Ibcb92b45d205b1464617919c1e16b1579dd0df40

--- rhosp/rhosp13_ziu/run.sh
+++ rhosp/rhosp13_ziu/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -eu
+set -x
+
+echo "" | tee -a ziu_run.log
+echo "$(date) start ziu's run.sh" | tee -a ziu_run.log
 
 #Check if it's running on undercloud node
 hostname=$(hostname -s)
-if [ "$hostname" != "undercloud" ]; then
+if [[ ${hostname} != *"undercloud"* ]]; then
    echo This script must be run on RHOSP13 undercloud node. Exiting
    exit 1
 fi
@@ -11,87 +15,95 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname "$my_file")"
 
 source $my_dir/../common/functions.sh
+source $my_dir/../common/set_ziu_variables.sh
 cd
 source rhosp-environment.sh
 source ziu.sh || true
 source stackrc
+
+printenv > ziu_env
+echo "$(date) env in ziu_env" | tee -a ziu_run.log
 
 #Checking mandatory env variables
 checkForVariable SSH_USER
 checkForVariable CONTRAIL_NEW_IMAGE_TAG
 checkForVariable CONTAINER_REGISTRY
 
-echo $(date) Preparing contrail images | tee -a run.log
+
+echo "$(date) preparing contrail images" | tee -a ziu_run.log
 mv contrail_containers.yaml contrail_containers.yaml.before_ziu
 ./contrail-tripleo-heat-templates/tools/contrail/import_contrail_container.sh \
     -f ./contrail_containers.yaml -r ${CONTAINER_REGISTRY} -t ${CONTRAIL_NEW_IMAGE_TAG}
+
+echo "prov_ip: $prov_ip" | tee -a ziu_run.log
+sed -i ./contrail_containers.yaml -e "s/192.168.24.1/${prov_ip}/"
+
 cat contrail_containers.yaml
 
+echo "$(date) download new contrail images and put them into local registry" | tee -a ziu_run.log
 #Download new contrail images and put them into local registry
 openstack overcloud container image upload --config-file ./contrail_containers.yaml
 
+echo "$(date) change misc_opts.yaml" | tee -a ziu_run.log
 #Changing misc_opts.yaml
 cp misc_opts.yaml misc_opts.yaml.before_ziu
 sed -i "s/${CONTRAIL_CONTAINER_TAG}/${CONTRAIL_NEW_IMAGE_TAG}/" misc_opts.yaml
 echo misc_opts.yaml was changed
 cat misc_opts.yaml
 
-#Distribute local mirrors configuration to overcloud nodes
-for ip in $(openstack server list -c Networks -f value | cut -d '=' -f2); do
-    scp /etc/yum.repos.d/local.repo $SSH_USER@$ip:
-    ssh $SSH_USER@$ip "sudo cp local.repo /etc/yum.repos.d/"
-done
+if [[ "$ENABLE_RHEL_REGISTRATION" == 'false' && "$USE_PREDEPLOYED_NODES" == 'false' ]]; then
+    echo "$(date) Distribute local mirrors configuration to overcloud node" | tee -a ziu_run.log
+    #Distribute local mirrors configuration to overcloud nodes
+    ssh_user=$(whoami)
+    for ip in $overcloud_prov_ip_list; do
+        scp /etc/yum.repos.d/local.repo $ssh_user@$ip:
+        ssh $ssh_user@$ip "sudo cp local.repo /etc/yum.repos.d/"
+    done
+fi
 
 ######################################################
 #                  ZIU                               #
 ######################################################
-
-echo $(date) openstack overcloud update prepare | tee -a run.log
 openstack overcloud update prepare --templates tripleo-heat-templates/ \
      --overcloud-ssh-user $SSH_USER \
-     --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
-     -e environment-rhel-registration.yaml \
-     -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml \
+     --roles-file $role_file \
+     -e docker_registry.yaml \
+     $rhel_reg_env_files \
+     $pre_deploy_nodes_env_files \
      -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-     -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
+     $network_env_files \
+     $storage_env_files \
      -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
+     $tls_env_files \
      -e misc_opts.yaml \
-     -e contrail-parameters.yaml \
-     -e docker_registry.yaml
+     -e contrail-parameters.yaml
 
-echo $(date) pre-syncing images to overcloud nodes. Stoping containers | tee -a run.log
+echo "$(date) pre-syncing images to overcloud nodes. stop containers" | tee -a ziu_run.log
 ~/contrail-tripleo-heat-templates/tools/contrail/update_contrail_preparation.sh
 
-#Upgrading contrail controllers
-for node in $(openstack server list --name overcloud-contrailcontroller -c Name -f value); do
-    echo $(date) Upgrading $node | tee -a run.log
+echo "$(date) upgrading nodes" | tee -a ziu_run.log
+
+echo "$(date)  $overcloud_instance_list" | tee -a ziu_run.log
+for node in $overcloud_instance_list; do
+    echo "$(date) Upgrading $node" | tee -a ziu_run.log
     openstack overcloud update run --ssh-user $SSH_USER --nodes $node
 done
 
-#Upgrading openstack controllers
-for node in $(openstack server list --name overcloud-controller -c Name -f value); do
-    echo $(date) Upgrading $node | tee -a run.log
-    openstack overcloud update run --ssh-user $SSH_USER --nodes $node
-done
-
-#Upgrading computes
-for node in $(openstack server list --name overcloud-novacompute -c Name -f value); do
-    echo $(date) Upgrading $node | tee -a run.log
-    openstack overcloud update run --ssh-user $SSH_USER --nodes $node
-done
-
-echo $(date) openstack overcloud update converge | tee -a run.log
-openstack overcloud update prepare --templates tripleo-heat-templates/ \
+echo "$(date) openstack overcloud update converge" | tee -a ziu_run.log
+openstack overcloud update converge --templates tripleo-heat-templates/ \
      --overcloud-ssh-user $SSH_USER \
-     --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
-     -e environment-rhel-registration.yaml \
-     -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml \
+     --roles-file $role_file \
+     -e docker_registry.yaml \
+     $rhel_reg_env_files \
+     $pre_deploy_nodes_env_files \
      -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-     -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
+     $network_env_files \
+     $storage_env_files \
      -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
+     $tls_env_files \
      -e misc_opts.yaml \
-     -e contrail-parameters.yaml \
-     -e docker_registry.yaml
+     -e contrail-parameters.yaml
 
-echo $(date) Successfully finished | tee -a run.log
+echo "$(date) Successfully finished" | tee -a ziu_run.log
+echo "$(date)  Successfully finished!" > it_works
 
