f32d089a074d271aa112419e43556d2c743fdb41
Arina Argentova <aargentova@progmaticlab.com>
2020-12-04 18:33:09 +0300

    [+]  deployment test for ziu in rhosp13 rhosp16
    partial-jira-bug: CEM-20714
    partial-jira-bug: CEM-20715
    
    Depends-On: I609ba9e2fa11d29aa63b1a9fca3f241ded299d63
    Depends-On: I871a4bd1440c9e608eff3096030b3f7edb7f45cf
    Change-Id: Ibcb92b45d205b1464617919c1e16b1579dd0df40

---
+++ rhosp/rhosp13_ziu/set_ziu_variables.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+source /tmp/test.env
+source rhosp-environment.sh
+
+tls_env_files=''
+if [[ -n "$ENABLE_TLS" ]] ; then
+  tls_env_files+=' -e tripleo-heat-templates/environments/contrail/contrail-tls.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/services/haproxy-public-tls-certmonger.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml'
+else
+  # use names even w/o tls case
+  tls_env_files+=' -e tripleo-heat-templates/environments/contrail/endpoints-public-dns.yaml'
+fi
+
+echo "$(date) tls_env_files are $tls_env_files" | tee -a ~/ziu_run.log
+rhel_reg_env_files=''
+if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
+  rhel_reg_env_files+=" -e environment-rhel-registration.yaml"
+  rhel_reg_env_files+=" -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml"
+fi
+
+echo "$(date) rhel_reg_env_files are $rhel_reg_env_files" | tee -a ~/ziu_run.log
+network_env_files=''
+if [[ ${ENABLE_NETWORK_ISOLATION+x} && "$ENABLE_NETWORK_ISOLATION" == true ]] ; then
+    network_env_files+=' -e tripleo-heat-templates/environments/network-isolation.yaml'
+    network_env_files+=' -e tripleo-heat-templates/environments/contrail/contrail-net.yaml'
+else
+    network_env_files+=' -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml'
+fi
+
+echo "$(date) network_env_files are $network_env_files" | tee -a ~/ziu_run.log
+storage_env_files=''
+if [[ ${overcloud_ceph_instance+x} && -n "$overcloud_ceph_instance" ]] ; then
+    storage_env_files+=' -e tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml'
+    storage_env_files+=' -e tripleo-heat-templates/environments/ceph-ansible/ceph-mds.yaml'
+fi
+echo "$(date) storage_env_files are $storage_env_files" | tee -a ~/ziu_run.log
+
+if [[ -z "$CONTROLLER_NODES" && -z "$AGENT_NODES" ]] ; then
+  role_file="$(pwd)/tripleo-heat-templates/roles/ContrailAio.yaml"
+else
+  role_file="$(pwd)/tripleo-heat-templates/roles_data_contrail_aio.yaml"
+fi
+
+echo "$(date) check for predeployed nodes [role_file is $role_file]" | tee -a ~/ziu_run.log
+pre_deploy_nodes_env_files=''
+if [[ ${USE_PREDEPLOYED_NODES+x} && "$USE_PREDEPLOYED_NODES" == true ]]; then
+  pre_deploy_nodes_env_files+=" --disable-validations"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-environment.yaml"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-bootstrap-environment-rhel.yaml"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-pacemaker-environment.yaml"
+  pre_deploy_nodes_env_files+=" -e ctlplane-assignments.yaml"
+  pre_deploy_nodes_env_files+=" -e hostname-map.yaml"
+
+  if [[ -z "$CONTROLLER_NODES" && -z "$AGENT_NODES" ]] ; then
+    export OVERCLOUD_ROLES="ContrailAio"
+    export ContrailAio_hosts="${overcloud_cont_prov_ip//,/ }"
+  else
+    export OVERCLOUD_ROLES="Controller Compute ContrailController"
+    export Controller_hosts="${overcloud_cont_prov_ip//,/ }"
+    export Compute_hosts="${overcloud_compute_prov_ip//,/ }"
+    export ContrailController_hosts="${overcloud_ctrlcont_prov_ip//,/ }"
+  fi
+fi
+
