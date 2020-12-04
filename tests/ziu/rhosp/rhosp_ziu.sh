8bfd2a74ed1b91c4807f7ee20d795c43d3decf97
Arina Argentova <aargentova@progmaticlab.com>
2020-12-04 18:33:09 +0300

    [+]  deployment test for ziu in rhosp13 rhosp16
    partial-jira-bug: CEM-20714
    partial-jira-bug: CEM-20715
    
    Depends-On: I609ba9e2fa11d29aa63b1a9fca3f241ded299d63
    Depends-On: I871a4bd1440c9e608eff3096030b3f7edb7f45cf
    Change-Id: Ibcb92b45d205b1464617919c1e16b1579dd0df40

---
+++ tests/ziu/rhosp/rhosp_ziu.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname "$my_file")"
+
+TF_CONFIG_DIR=${TF_CONFIG_DIR:-"$HOME/.tf"}
+source /tmp/test.env
+
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY_ORIGINAL"
+
+export SSH_OPTIONS=${SSH_OPTIONS:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
+export SSH_USER=tripleo-admin
+
+function wait_cmd_success() {
+    i=0
+    while ! eval $3; do
+        sleep $1
+        printf "."
+        i=$((i + 1))
+        if (( i >= $2 )); then
+            echo -e "\nERROR: wait failed in $((i*$1))s"
+            return 1
+        fi
+    done
+    echo -e "\nINFO: done in $((i*$1))s"
+    return 0
+}
+
+function check_tf_active() {
+    local machine
+    local line=
+    for machine in $(echo "$AGENT_NODES,$CONTROLLER_NODES" | sed "s/,/ /g"); do
+        if ! ssh $SSH_OPTIONS $machine "command -v contrail-status" 2>/dev/null ; then
+            return 1
+        fi
+        for line in $(ssh $SSH_OPTIONS $machine "sudo contrail-status" 2>/dev/null | egrep ": " | grep -v "WARNING" | awk '{print $2}'); do
+            if [ "$line" != "active" ] && [ "$line" != "backup" ] ; then
+                return 1
+            fi
+        done
+    done
+    return 0
+}
+
+if [ "$OPENSTACK_VERSION" == "queens" ]; then
+    RHOSP_DIR="rhosp13_ziu"
+elif [ "$OPENSTACK_VERSION" == "train" ]; then
+    RHOSP_DIR="rhosp16_ziu"
+fi
+
+$my_dir/../../../rhosp/$RHOSP_DIR/run.sh
+
+run_exit_code=$?
+
+if [[ $run_exit_code != 0 ]]; then
+    exit 1
+fi
+
+if ! wait_cmd_success 120 4 "check_tf_active" ; then
+    echo "ERROR: tf is not active after ziu"
+    exit 1
+fi
+
