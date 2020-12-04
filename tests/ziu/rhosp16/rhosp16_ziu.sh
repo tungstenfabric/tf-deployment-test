f1c13f280cf4d5e83e61a62c292cba0e19ab87bb
Arina Argentova <aargentova@progmaticlab.com>
2020-12-04 18:33:09 +0300

    [+]  deployment test for ziu in rhosp13
    partial-jira-bug: CEM-20714
    partial-jira-bug: CEM-20715
    
    Depends-On: I609ba9e2fa11d29aa63b1a9fca3f241ded299d63
    Change-Id: Ibcb92b45d205b1464617919c1e16b1579dd0df40

---
+++ tests/ziu/rhosp16/rhosp16_ziu.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname "$my_file")"
+
+TF_CONFIG_DIR=${TF_CONFIG_DIR:-"$HOME/.tf"}
+source /tmp/test.env
+
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY_ORIGINAL"
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG_ORIGINAL$TAG_SUFFIX"
+export SSH_OPTIONS=${SSH_OPTIONS:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
+
+function wait_cmd_success() {
+    i=0
+    while ! eval $3; do
+        sleep $1
+        printf "."
+        i=$((i + 1))
+        if (( i >= $2 )); then
+            echo -e "\nERROR: wait failed in $((i*$1))s"
+            return 1
+        fi
+    done
+    echo -e "\nINFO: done in $((i*$1))s"
+    return 0
+}
+
+function check_tf_active() {
+  local machine
+  local line=
+  for machine in $(echo "$AGENT_NODES,$CONTROLLER_NODES" | sed "s/,/ /g"); do
+    if ! ssh $SSH_OPTIONS $machine "command -v contrail-status" 2>/dev/null ; then
+      return 1
+    fi
+    for line in $(ssh $SSH_OPTIONS $machine "sudo contrail-status" 2>/dev/null | egrep ": " | grep -v "WARNING" | awk '{print $2}'); do
+      if [ "$line" != "active" ] && [ "$line" != "backup" ] ; then
+        return 1
+      fi
+    done
+  done
+  return 0
+}
+
+./$mydir/../../../rhosp/rhosp16_ziu/run.sh
+
+if ! wait_cmd_success 300 120 "check_tf_active" ; then
+    echo "ERROR: tf is not active after ziu"
+    exit 1
+fi
