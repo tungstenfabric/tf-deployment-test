8ff324e2455e8868edd6e96c617bedb6dc1d1b49
Arina Argentova <aargentova@progmaticlab.com>
2020-12-04 18:33:09 +0300

    [+]  deployment test for ziu in rhosp13 rhosp16
    partial-jira-bug: CEM-20714
    partial-jira-bug: CEM-20715
    
    Depends-On: I609ba9e2fa11d29aa63b1a9fca3f241ded299d63
    Depends-On: I871a4bd1440c9e608eff3096030b3f7edb7f45cf
    Change-Id: Ibcb92b45d205b1464617919c1e16b1579dd0df40

---
+++ rhosp/common/set_common_ziu_var.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source /tmp/test.env
+source rhosp-environment.sh
+
+overcloud_prov_ip_list=''
+overcloud_instance_list=''
+for nodes in "$CONTROLLER_NODES" "$OPENSTACK_CONTROLLER_NODES" "$AGENT_NODES"; do
+    for node in $(echo $nodes | sed "s/,/ /g"); do
+        cutted_node="$(echo $node | cut -d "." -f1)"
+        if [[ $overcloud_prov_ip_list != *"$node"* ]]; then
+            overcloud_prov_ip_list+="$node "
+        fi
+        if [[ $overcloud_instance_list != *"$cutted_node"* ]]; then
+            overcloud_instance_list+="$cutted_node "
+        fi
+    done
+done
+
+echo "$(date) overcloud_prov_ip_list:  $overcloud_prov_ip_list" | tee -a ~/ziu_run.log
+echo "$(date) overcloud_instance_list: $overcloud_instance_list" | tee -a ~/ziu_run.log
