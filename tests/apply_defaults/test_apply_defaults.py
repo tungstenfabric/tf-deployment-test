86500de33d45428e180cbfef68873d83d29e9999
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-12-09 16:10:07 +0300

    test for juju action apply-defaults WIP (without JujuAction)
    Closes-Jira-Bug: CEM-19841
    Change-Id: I072010fd61bd243c27d0fcc41fed81c407d18999

--- tests/apply_defaults/test_apply_defaults.py
+++ tests/apply_defaults/test_apply_defaults.py
@@ -5,15 +5,16 @@ from testtools.testcase import attr, WithAttributes
 
 from common.deployment_test_case import DeploymentTestCase
 from common.utils.vnc_api import VncApiProxy
+from common.fixtures.host_fixture import HostFixture
 
 # TODO: allow to set level in config
 # TODO: move this to single base test class
 logging.basicConfig(level=logging.INFO)
 
 
-class ApplyDefaultsTests(WithAttributes, DeploymentTestCase):
+class ApplyDefaultsTests(WithAttributes, DeploymentTestCase, JujuAction):
 
-    @attr("all-deployers", "kubernetes")
+    @attr("ansible", "kubernetes")
     def test_apply_defaults(self):
         self.logger = logging.getLogger(__name__ + '.test_apply_defaults')
         self.logger.info('Go test check')
@@ -45,3 +46,46 @@ class ApplyDefaultsTests(WithAttributes, DeploymentTestCase):
         finally:
             # TODO: rework this to resources that must be restored
             vnc_api_client.set_encap_priorities(original_encap_priorities)
+
+    @attr("juju", "all-orchestrators")
+    def test_juju_action_apply_defaults(self):
+        self.logger = logging.getLogger(__name__ + '.test_juju_apply_defaults')
+        self.logger.info('Go test check')
+        host_fixture = self.useFixture(HostFixture())
+        vnc_api_client = VncApiProxy()
+        juju_config_cmd = 'juju config contrail-controller encap-priority'
+        apply_cmd = 'juju run-action contrail-controller/0 apply-defaults --wait'
+
+        original_api_encap_priorities = vnc_api_client.get_encap_priorities()
+        original_juju_encap_priorities = host_fixture.exec_command(juju_config_cmd)[0].strip().split(",")
+        self.logger.info(f'original_juju: {original_juju_encap_priorities} ; original_api: {original_api_encap_priorities}')
+        try:
+            assert original_juju_encap_priorities == original_api_encap_priorities, "ERROR: juju encap_priority differs from api encap_priority"
+
+            new_juju_encap_priorities = ['VXLAN', 'MPLSoUDP', 'MPLSoGRE']
+            if new_juju_encap_priorities == original_juju_encap_priorities:
+                new_juju_encap_priorities = ['VXLAN', 'MPLSoGRE', 'MPLSoUDP']
+            encaps = new_juju_encap_priorities
+            host_fixture.exec_command(f'{juju_config_cmd}={encaps[0]},{encaps[1]},{encaps[2]}')
+            current_juju_encap_priorities = host_fixture.exec_command(juju_config_cmd)[0].strip().split(",")
+            self.logger.info(f'new_juju: {new_juju_encap_priorities} ; current_juju: {current_juju_encap_priorities}')
+            assert new_juju_encap_priorities == current_juju_encap_priorities, "ERROR: encap_priority was not set or set incorrectly by juju"
+
+            current_api_encap_priorities = vnc_api_client.get_encap_priorities()
+            self.logger.info(f'original_api: {original_api_encap_priorities} ; current_api_before: {current_api_encap_priorities}')
+            assert original_api_encap_priorities == current_api_encap_priorities, "ERROR: api encap_priority was changed"
+
+            self.logger.info(f'result_apply_defaults: {host_fixture.exec_command(apply_cmd)[0]}')
+
+            current_api_encap_priorities = vnc_api_client.get_encap_priorities()
+            self.logger.info(f'new_juju: {new_juju_encap_priorities} ; current_api_after: {current_api_encap_priorities}')
+            assert new_juju_encap_priorities == current_api_encap_priorities, "ERROR: api encap_priority was not set or set incorrectly"
+        finally:
+            default_juju_encap_priorities = ['MPLSoUDP', 'MPLSoGRE', 'VXLAN']
+            current_juju_encap_priorities = host_fixture.exec_command(juju_config_cmd)[0].strip().split(",")
+            if default_juju_encap_priorities != current_juju_encap_priorities:
+                encaps = default_juju_encap_priorities
+                host_fixture.exec_command(f'{juju_config_cmd}={encaps[0]},{encaps[1]},{encaps[2]}')
+                self.logger.info(f'result_apply_defaults_for_default: {host_fixture.exec_command(apply_cmd)[0]}')
+
+        self.logger.info('juju_action_apply_default test: PASSED')
