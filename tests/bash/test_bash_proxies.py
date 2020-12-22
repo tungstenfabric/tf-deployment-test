f946476866025103bd6d6d0c16f0d89e185ac6ac
tikitavi <rtikitavi@gmail.com>
2020-12-22 23:28:25 +0300

    add test for nrpe in juju
    Partialy-Jira-bug: CEM-20834
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: I074881616dafda07174039595f2a4eb640b0ad4a
    Depends-on: I7eacd8120e74077bb18dcc3b40ed3a9d03a8dfa6

--- tests/bash/test_bash_proxies.py
+++ tests/bash/test_bash_proxies.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import testtools
 from testtools.testcase import attr, WithAttributes
 
 from common.deployment_test_case import DeploymentTestCase
@@ -26,4 +27,14 @@ class BashTests(WithAttributes, DeploymentTestCase):
     def test_k8s_auth_keystone(self):
         self.logger = logging.getLogger(__name__ + '.k8s_auth_keystone')
         file_name = os.path.join(CURRENT_DIRECTORY, 'k8s_auth_keystone.sh')
+        self.run_test_remotely(file_name)
+
+    @attr("juju", "all-orchestrators")
+    def test_juju_nrpe(self):
+        if os.getenv("ENABLE_NAGIOS", 'false') != 'true':
+            msg = "Skipped as nrpe isn't enabled"
+            raise testtools.TestCase.skipException(msg)
+
+        self.logger = logging.getLogger(__name__ + '.juju_nrpe')
+        file_name = os.path.join(CURRENT_DIRECTORY, 'juju_nrpe.sh')
         self.run_test_remotely(file_name)
