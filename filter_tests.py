e059673e9220b3134e43091b95fe2f6fe7515cad
tikitavi <rtikitavi@gmail.com>
2020-12-22 20:31:29 +0300

    fix filtering tests by tags
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Partialy-jira-bug: CEM-20861
    Change-Id: I01eb314b4d54f54afaeefa33ca9d217987afa696

---
+++ filter_tests.py
@@ -0,0 +1,38 @@
+import os
+import re
+import sys
+
+orchestrator_tag = os.environ.get("ORCHESTRATOR")
+deployer_tag = os.environ.get('DEPLOYER')
+additional_tags = os.environ.get('DEPLOYMENT_TEST_TAGS', set())
+if not isinstance(additional_tags, set):
+    additional_tags = set(additional_tags.split(','))
+
+
+for line in sys.stdin:
+    # get tags for every test
+    try:
+        test_tags = re.search('\[(.*)\]', line).group(1)
+    except:
+        continue
+    test_tags_list = test_tags.split(',')
+
+    # filter for orchestrator
+    if orchestrator_tag in test_tags_list:
+        test_tags_list.remove(orchestrator_tag)
+    elif 'all-orchestrators' in test_tags_list:
+        test_tags_list.remove('all-orchestrators')
+    else:
+        continue
+
+    # filter for deployer
+    if deployer_tag in test_tags_list:
+        test_tags_list.remove(deployer_tag)
+    elif 'all-deployers' in test_tags_list:
+        test_tags_list.remove('all-deployers')
+    else:
+        continue
+
+    # filter for additional
+    if additional_tags == set(test_tags_list):
+        print(line.strip('\n'))
