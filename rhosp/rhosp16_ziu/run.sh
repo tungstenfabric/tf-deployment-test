f26f1e113f537cf24531f7cf2bb9cce758a8ac3e
Gleb Galkin <gleb108@gmail.com>
2020-12-24 13:23:59 +0300

    [+]  RHOSP16 ziu process and test
    partial-jira-bug: CEM-20116
    partial-jira-bug: CEM-20715
    partial-jira-bug: CEM-20714
    
    Depends-On: I0dda1cde09a97135f8525f8470b580f57f24b94b
    Change-Id: I6165af134626d5b34ac2ddfd5b20854ebdca240f

--- rhosp/rhosp16_ziu/run.sh
+++ rhosp/rhosp16_ziu/run.sh
@@ -13,35 +13,42 @@ set -a
 source $my_dir/../common/functions.sh
 source $my_dir/../common/set_common_ziu_var.sh
 source $my_dir/set_ziu_variables.sh
+source /tmp/test.env
+
 cd
 source rhosp-environment.sh
 source stackrc
-source /tmp/test.env
-
 printenv > ziu_env
-
+echo "$(date) env in ziu_env" | tee -a ziu_run.log
 #Checking mandatory env variables
 checkForVariable SSH_USER
+checkForVariable CONTRAIL_NEW_IMAGE_TAG
 checkForVariable CONTAINER_REGISTRY_ORIGINAL
 checkForVariable CONTRAIL_CONTAINER_TAG
-checkForVariable CONTRAIL_NEW_IMAGE_TAG
 checkForVariable USE_PREDEPLOYED_NODES
 checkForVariable ENABLE_RHEL_REGISTRATION
+
+
+########################### UPLOAD #####################################
 
 mv contrail_containers.yaml contrail_containers.yaml.before_ziu
 ./contrail-tripleo-heat-templates/tools/contrail/import_contrail_container.sh \
     -f ./contrail_containers.yaml -r ${CONTAINER_REGISTRY_ORIGINAL} -t ${CONTRAIL_NEW_IMAGE_TAG}
-
 sed -i ./contrail_containers.yaml -e "s/192.168.24.1/${prov_ip}/"
 
 cat contrail_containers.yaml
 
-#Download new contrail images and put them into local registry
-openstack overcloud container image upload --config-file ./contrail_containers.yaml
+sudo openstack overcloud container image upload --config-file ./contrail_containers.yaml
 
 #Changing misc_opts.yaml
 cp misc_opts.yaml misc_opts.yaml.before_ziu
 sed -i "s/${CONTRAIL_CONTAINER_TAG}/${CONTRAIL_NEW_IMAGE_TAG}/" misc_opts.yaml
+
+if [[ "$USE_PREDEPLOYED_NODES" == 'true' ]]; then
+   echo "  SkipRhelEnforcement: true" >> misc_opts.yaml
+fi
+
+echo misc_opts.yaml was changed
 cat misc_opts.yaml
 
 if [[ "$ENABLE_RHEL_REGISTRATION" == 'false' && "$USE_PREDEPLOYED_NODES" == 'false' ]]; then
@@ -52,42 +59,52 @@ if [[ "$ENABLE_RHEL_REGISTRATION" == 'false' && "$USE_PREDEPLOYED_NODES" == 'fal
     done
 fi
 
+
 ######################################################
 #                  ZIU                               #
 ######################################################
+
+./tripleo-heat-templates/tools/process-templates.py --clean \
+  -r $role_file \
+  -p tripleo-heat-templates/
+
+./tripleo-heat-templates/tools/process-templates.py \
+  -r $role_file \
+  -p tripleo-heat-templates/
+
+
 openstack overcloud update prepare --templates tripleo-heat-templates/ \
-     --overcloud-ssh-user tripleo-admin \
-     --roles-file $role_file \
-     -e docker_registry.yaml \
-     $rhel_reg_env_files \
-     $pre_deploy_nodes_env_files \
-     -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-     $network_env_files \
-     $storage_env_files \
-     -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-     $tls_env_files \
-     -e misc_opts.yaml \
-     -e contrail-parameters.yaml
+  --stack overcloud --libvirt-type kvm \
+  --roles-file $role_file \
+  -e overcloud_containers.yaml \
+  $rhel_reg_env_files \
+  $pre_deploy_nodes_env_files \
+  -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
+  $network_env_files \
+  -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
+  $tls_env_files \
+  -e misc_opts.yaml \
+  -e contrail-parameters.yaml \
+  -e containers-prepare-parameter.yaml
 
 ~/contrail-tripleo-heat-templates/tools/contrail/update_contrail_preparation.sh
 
 for node in $overcloud_instance_list; do
-    openstack overcloud update run --ssh-user tripleo-admin --nodes $node
+    openstack overcloud update run --ssh-user tripleo-admin --limit $node
 done
 
 openstack overcloud update converge --templates tripleo-heat-templates/ \
-     --overcloud-ssh-user tripleo-admin \
-     --roles-file $role_file \
-     -e docker_registry.yaml \
-     $rhel_reg_env_files \
-     $pre_deploy_nodes_env_files \
-     -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-     $network_env_files \
-     $storage_env_files \
-     -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-     $tls_env_files \
-     -e misc_opts.yaml \
-     -e contrail-parameters.yaml
+  --stack overcloud --libvirt-type kvm \
+  --roles-file $role_file \
+  -e overcloud_containers.yaml \
+  $rhel_reg_env_files \
+  $pre_deploy_nodes_env_files \
+  -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
+  $network_env_files \
+  -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
+  $tls_env_files \
+  -e misc_opts.yaml \
+  -e contrail-parameters.yaml \
+  -e containers-prepare-parameter.yaml
 
 echo "$(date) Successfully finished"
-
