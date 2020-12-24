ea004b26cbefbc81c946e8b3633ff5af442185cc
Arina Argentova <aargentova@progmaticlab.com>
2020-12-24 15:07:48 +0300

    [~]  remove TAG_SUFFIX
    partial-jira-bug: CEM-20714
    partial-jira-bug: CEM-20715
    Change-Id: I6165af134626d5b34ac2ddfd5b20854ebdca240f

--- rhosp/rhosp13_ziu/run.sh
+++ rhosp/rhosp13_ziu/run.sh
@@ -18,20 +18,22 @@ source $my_dir/../common/set_common_ziu_var.sh
 source $my_dir/set_ziu_variables.sh
 cd
 source rhosp-environment.sh
-source ziu.sh || true
 source stackrc
+source /tmp/test.env
 
 printenv > ziu_env
 echo "$(date) env in ziu_env" | tee -a ziu_run.log
 
 #Checking mandatory env variables
 checkForVariable SSH_USER
-checkForVariable CONTAINER_REGISTRY
+checkForVariable CONTAINER_REGISTRY_ORIGINAL
+checkForVariable CONTRAIL_CONTAINER_TAG
+checkForVariable CONTRAIL_NEW_IMAGE_TAG
 
 echo "$(date) preparing contrail images" | tee -a ziu_run.log
 mv contrail_containers.yaml contrail_containers.yaml.before_ziu
 ./contrail-tripleo-heat-templates/tools/contrail/import_contrail_container.sh \
-    -f ./contrail_containers.yaml -r ${CONTAINER_REGISTRY} -t ${CONTRAIL_CONTAINER_TAG}
+    -f ./contrail_containers.yaml -r ${CONTAINER_REGISTRY_ORIGINAL} -t ${CONTRAIL_NEW_IMAGE_TAG}
 
 echo "$(date) prov_ip for contrail_containers.yaml: $prov_ip" | tee -a ziu_run.log
 sed -i ./contrail_containers.yaml -e "s/192.168.24.1/${prov_ip}/"
@@ -45,17 +47,16 @@ openstack overcloud container image upload --config-file ./contrail_containers.y
 echo "$(date) change misc_opts.yaml" | tee -a ziu_run.log
 #Changing misc_opts.yaml
 cp misc_opts.yaml misc_opts.yaml.before_ziu
-sed -i "s/${CONTRAIL_CONTAINER_TAG_ORIGINAL}${TAG_SUFFIX}/${CONTRAIL_CONTAINER_TAG}/" misc_opts.yaml
+sed -i "s/${CONTRAIL_CONTAINER_TAG}/${CONTRAIL_NEW_IMAGE_TAG}/" misc_opts.yaml
 echo "$(date) misc_opts.yaml was changed" | tee -a ziu_run.log
 cat misc_opts.yaml
 
 if [[ "$ENABLE_RHEL_REGISTRATION" == 'false' && "$USE_PREDEPLOYED_NODES" == 'false' ]]; then
     echo "$(date) Distribute local mirrors configuration to overcloud nodes" | tee -a ziu_run.log
     #Distribute local mirrors configuration to overcloud nodes
-    ssh_user=$(whoami)
     for ip in $overcloud_prov_ip_list; do
-        scp /etc/yum.repos.d/local.repo $ssh_user@$ip:
-        ssh $ssh_user@$ip "sudo cp local.repo /etc/yum.repos.d/"
+        scp /etc/yum.repos.d/local.repo $SSH_USER@$ip:
+        ssh $SSH_USER@$ip "sudo cp local.repo /etc/yum.repos.d/"
     done
 fi
 
@@ -63,7 +64,7 @@ fi
 #                  ZIU                               #
 ######################################################
 openstack overcloud update prepare --templates tripleo-heat-templates/ \
-     --overcloud-ssh-user $SSH_USER \
+     --overcloud-ssh-user tripleo-admin \
      --roles-file $role_file \
      -e docker_registry.yaml \
      $rhel_reg_env_files \
@@ -83,12 +84,12 @@ echo "$(date) upgrading nodes '$overcloud_instance_list'" | tee -a ziu_run.log
 
 for node in $overcloud_instance_list; do
     echo "$(date) Upgrading $node" | tee -a ziu_run.log
-    openstack overcloud update run --ssh-user $SSH_USER --nodes $node
+    openstack overcloud update run --ssh-user tripleo-admin --nodes $node
 done
 
 echo "$(date) openstack overcloud update converge" | tee -a ziu_run.log
 openstack overcloud update converge --templates tripleo-heat-templates/ \
-     --overcloud-ssh-user $SSH_USER \
+     --overcloud-ssh-user tripleo-admin \
      --roles-file $role_file \
      -e docker_registry.yaml \
      $rhel_reg_env_files \
