95209d227e1e4174e17044af232445b611898f01
alexey-mr <alexey.morlang@gmail.com>
2020-12-28 20:51:52 +0300

    Reworked logging
    Change-Id: I11af6e61228e5f0bfc3d7c330c3c384ed550f8f0

--- testrunner.sh
+++ testrunner.sh
@@ -37,8 +37,8 @@ if [ -d $scriptdir/tf-deployment-test ]; then
 fi
 
 TF_DEPLOYMENT_TEST_IMAGE="${TF_DEPLOYMENT_TEST_IMAGE:-${CONTAINER_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG}}"
-echo "INFO: command to run: sudo docker run --privileged=true --rm=true -i $vol_opts --network host $TF_DEPLOYMENT_TEST_IMAGE"
-sudo docker run --privileged=true --rm=true -i $vol_opts --network host $TF_DEPLOYMENT_TEST_IMAGE || res=1
+echo "INFO: command to run: sudo docker run --privileged=true --rm=true -t $vol_opts --network host $TF_DEPLOYMENT_TEST_IMAGE"
+sudo docker run --privileged=true --rm=true -t $vol_opts --network host $TF_DEPLOYMENT_TEST_IMAGE || res=1
 
 pushd $scriptdir/output
 tar -cvf $WORKSPACE/logs.tar logs
