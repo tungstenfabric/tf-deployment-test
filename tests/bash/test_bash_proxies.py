774e4a266a7c9fdabc0aaff754c9ab852883d2a4
alexey-mr <alexey.morlang@gmail.com>
2020-12-28 20:51:52 +0300

    Reworked logging
    Change-Id: I11af6e61228e5f0bfc3d7c330c3c384ed550f8f0

--- tests/bash/test_bash_proxies.py
+++ tests/bash/test_bash_proxies.py
@@ -1,37 +1,21 @@
-import os
-import logging
-from testtools.testcase import attr, WithAttributes
+from testtools.testcase import attr
 from testtools import skipIf
-
 from common.deployment_test_case import DeploymentTestCase
 
-# TODO: allow to read log level from config
-# TODO: move log level set into base class
-logging.basicConfig(level=logging.INFO)
 
-
-CURRENT_DIRECTORY = "tests/bash"
-
-
-class BashTests(WithAttributes, DeploymentTestCase):
+class BashTests(DeploymentTestCase):
     # k8s_manifests-kubernetes attr means deployer is k8s_manifests
     # and orchestrator  is kubernetes
     # this test will be run for k8s_manifests deployer and kubernetes orchestrator
     @attr("k8s_manifests", "kubernetes")
     def test_check_agent_status(self):
-        self.logger = logging.getLogger(__name__ + '.check_agent_status')
-        file_name = os.path.join(CURRENT_DIRECTORY, 'check_agent_status.sh')
-        self.run_test_remotely(file_name)
+        self.run_test_remotely('tests/bash/check_agent_status.sh')
 
     @attr("juju", "hybrid")
     def test_k8s_keystone_auth(self):
-        self.logger = logging.getLogger(__name__ + '.k8s_keystone_auth')
-        file_name = os.path.join(CURRENT_DIRECTORY, 'k8s_keystone_auth.sh')
-        self.run_test_remotely(file_name)
+        self.run_test_remotely('tests/bash/k8s_keystone_auth.sh')
 
     @skipIf(os.getenv("ENABLE_NAGIOS", 'false') != 'true', "Skipped as nrpe isn't enabled")
     @attr("juju", "all-orchestrators")
     def test_juju_nrpe(self):
-        self.logger = logging.getLogger(__name__ + '.juju_nrpe')
-        file_name = os.path.join(CURRENT_DIRECTORY, 'juju_nrpe.sh')
-        self.run_test_remotely(file_name)
+        self.run_test_remotely('tests/bash/juju_nrpe.sh')
--- tests/ziu/rhosp/test_rhosp_ziu.py
+++ tests/ziu/rhosp/test_rhosp_ziu.py
@@ -1,20 +1,8 @@
-import os
-import logging
-from testtools.testcase import attr, WithAttributes
-
+from testtools.testcase import attr
 from common.deployment_test_case import DeploymentTestCase
 
-# TODO: allow to read log level from config
-# TODO: move log level set into base class
-logging.basicConfig(level=logging.INFO)
 
-
-CURRENT_DIRECTORY = 'tests/ziu/rhosp'
-
-
-class RhospZiuTests(WithAttributes, DeploymentTestCase):
+class RhospZiuTests(DeploymentTestCase):
     @attr("rhosp", "openstack", "ziu")
     def test_rhosp_ziu(self):
-        self.logger = logging.getLogger(__name__ + '.rhosp_ziu')
-        file_name = os.path.join(CURRENT_DIRECTORY, 'rhosp_ziu.sh')
-        self.run_test_remotely(file_name)
+        self.run_test_remotely('tests/ziu/rhosp/rhosp_ziu.sh')
