508b9311979b894d93508b578a10d46770614759
alexey-mr <alexey.morlang@gmail.com>
2020-12-28 20:51:52 +0300

    Reworked logging
    Change-Id: I11af6e61228e5f0bfc3d7c330c3c384ed550f8f0

--- common/fixtures/host_fixture.py
+++ common/fixtures/host_fixture.py
@@ -1,10 +1,8 @@
 import fixtures
-import logging
 import io
 import os
 import paramiko
 import select
-import sys
 from subprocess import check_call
 
 
@@ -17,10 +15,10 @@ class HostFixture(fixtures.Fixture):
         "-o PasswordAuthentication=no"
     ])
 
-    def __init__(self, ssh_host=None, ssh_user=None):
-        self.logger = logging.getLogger(__name__ + '.HostFixture')
-        self.ssh_user = ssh_user or os.getenv("SSH_USER")
-        self.ssh_host = ssh_host or os.getenv("SSH_HOST")
+    def __init__(self, ssh_host, ssh_user, logger):
+        self.ssh_host = ssh_host
+        self.ssh_user = ssh_user
+        self.logger = logger
 
     def _rsync_data(self, path):
         if not self.ssh_user or not self.ssh_host:
@@ -43,55 +41,58 @@ class HostFixture(fixtures.Fixture):
         ssh.connect(self.ssh_host, username=self.ssh_user, timeout=5)
         return ssh
 
+    def _write_stream(self, stream, bdata):
+        if bdata and len(bdata) == 0:
+            self.logger.debug(bdata.decode("utf-8", "backslashreplace").strip())
+            if stream:
+                stream.buffer.write(bdata)
+                # do write sync, to allow easier debugging
+                stream.flush()
+
     def get_remote_path(self, local_file_path):
         return os.path.join("/tmp/tf-deployment-test", local_file_path)
 
     def exec_command(self, command, fout=None, ferr=None, timeout=5):
-        ssh = self._get_connection()
-        self.logger.debug("Start command over ssh command='%s'" % (command))
-        stdin, stdout, stderr = ssh.exec_command(command)
-        self.logger.debug("Command started, waiting results...")
-        # get the shared channel for stdout/stderr/stdin
-        channel = stdout.channel
-        # we do not need stdin.
-        stdin.close()
-        # indicate that we're not going to write to that channel anymore
-        channel.shutdown_write()
-        _fout = fout if fout else sys.stdout
-        _ferr = ferr if ferr else sys.stderr
-        _fout.buffer.write(channel.recv(len(channel.in_buffer)))
-        _fout.flush()
-        while not channel.closed or \
-                channel.recv_ready() or \
-                channel.recv_stderr_ready():
-            got_chunk = False
-            readq, _, _ = select.select([channel], [], [], timeout)
-            for c in readq:
-                if c.recv_ready():
-                    _fout.buffer.write(channel.recv(len(c.in_buffer)))
-                    _fout.flush()
-                    got_chunk = True
-                if c.recv_stderr_ready():
-                    _ferr.buffer.write(channel.recv_stderr(len(c.in_stderr_buffer)))
-                    _ferr.flush()
-                    got_chunk = True
-            # if new data comes or data were read, try read more
-            if got_chunk or channel.recv_stderr_ready() or channel.recv_ready():
-                continue
-            if channel.exit_status_ready():
-                # no more data and process ended
-                break
-        # indicate that we're not going to read from this channel anymore
-        channel.shutdown_read()
-        channel.close()
-        stdout.close()
-        stderr.close()
-        res = channel.recv_exit_status()
-        ssh.close()
-        self.logger.debug("Command finished, res=%s" % (res))
-        if res:
-            msg = f'ERROR: SSH Command "{command}" failed with exit code {res}'
-            raise Exception(msg)
+        with self._get_connection() as ssh:
+            self.logger.debug("Start command over ssh command='%s'" % (command))
+            stdin, stdout, stderr = ssh.exec_command(command)
+            self.logger.debug("Command started, waiting result...")
+            # get the shared channel for stdout/stderr/stdin
+            channel = stdout.channel
+            # indicate that we're not going to write to that channel anymore
+            stdin.close()
+            channel.shutdown_write()
+            while not channel.closed or \
+                    channel.recv_ready() or \
+                    channel.recv_stderr_ready():
+                got_chunk = False
+                readq, _, _ = select.select([channel], [], [], timeout)
+                for c in readq:
+                    if c.recv_ready():
+                        self._write_stream(
+                            fout,
+                            channel.recv(len(c.in_buffer)))
+                        got_chunk = True
+                    if c.recv_stderr_ready():
+                        self._write_stream(
+                            ferr,
+                            channel.recv_stderr(len(c.in_stderr_buffer)))
+                        got_chunk = True
+                # if new data comes or data were read, try read more
+                if got_chunk or \
+                        channel.recv_stderr_ready() or \
+                        channel.recv_ready():
+                    continue
+                if channel.exit_status_ready():
+                    # no more data and process ended
+                    break
+            # indicate that we're not going to read from this channel anymore
+            channel.shutdown_read()
+            res = channel.recv_exit_status()
+            self.logger.debug("Command finished, res=%s" % (res))
+            if res:
+                msg = 'ERROR: SSH Command "%s" failed with exit code %s'
+                raise Exception(msg % (command, res))
 
     def exec_command_result(self, command, timeout=5):
 
@@ -105,7 +106,6 @@ class HostFixture(fixtures.Fixture):
             return fout.getvalue().decode('utf-8')
 
     def copy_local_file_to_remote(self, local_path, remote_path):
-        ssh = self._get_connection()
-        with ssh.open_sftp() as ftp_client:
-            ftp_client.put(local_path, remote_path)
-        ssh.close()
+        with self._get_connection() as ssh:
+            with ssh.open_sftp() as ftp_client:
+                ftp_client.put(local_path, remote_path)
