a260507abb6b7addd25f60c39ece7be528906549
Andrey Pavlov <andrey.mp@gmail.com>
2021-05-07 07:25:24 +0000

    Fix ZIU for juju
    Change-Id: I8685b198333775dfc0ac3faf1f7be12981b8b1c5

--- tests/ziu/juju/juju_ziu.sh
+++ tests/ziu/juju/juju_ziu.sh
@@ -9,26 +9,36 @@ export CONTAINER_REGISTRY="$CONTAINER_REGISTRY_ORIGINAL"
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG_ORIGINAL"
 export SSH_OPTIONS=${SSH_OPTIONS:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
 
-CONTROLLERS_COUNT=`echo "$( echo $CONTROLLER_NODES | tr ',' ' ' )" | awk -F ' ' '{print NF}'`
-analyticsdb_enabled=$(( $(juju status | cut -d " " -f 1 | grep -q tf-analyticsdb; echo $?) == 0 ))
-status_nodes=$(( $CONTROLLERS_COUNT * (2 + $analyticsdb_enabled) ))
+function ziu_status_for_pattern() {
+    # juju 2.9.0 started to return similar messages in applications as in units - cur app section first
+    if [[ $(juju status | grep -A 2000 "Unit *Workload" | grep "$1" | wc -l) != "$units_count" ]]; then
+        return 1
+    fi
+    return 0
+}
 
-function ziu_status() {
-    (( $(juju status | grep "$1" | wc -l) - $status_nodes ))
+function juju_status_absent_pattern() {
+    # juju 2.9.0 started to return similar messages in applications as in units - cur app section first
+    if juju status | grep -A 2000 "Unit *Workload" | grep -q "$1" ; then
+        return 1
+    fi
+    return 0
 }
 
 function wait_cmd_success() {
-    i=0
-    while eval $3; do
+    local i=0
+    while ! eval $3; do
         sleep $1
         printf "."
         i=$((i + 1))
         if (( i >= $2 )); then
-            echo -e "\nERROR: wait failed in $((i*$1))s"
+            echo ''
+            echo "ERROR: wait failed in $((i*$1))s"
             return 1
         fi
     done
-    echo -e "\nINFO: done in $((i*$1))s"
+    echo ''
+    echo "INFO: done in $((i*$1))s"
     return 0
 }
 
@@ -40,38 +50,61 @@ function sync_time() {
     done
 }
 
+echo "INFO: current env  $(date)"
+env|sort
+echo ''
+
+controllers_count=$(echo $CONTROLLER_NODES | tr ',' ' ' | wc -w)
+units_count=$controllers_count
+if [[ ${LEGACY_ANALYTICS_ENABLE,,} == 'true' ]]; then
+    units_count=$((3 * controllers_count))
+fi
+echo "INFO: Count of units in control plane = $units_count. Start ZIU...  $(date)"
+
 juju run-action tf-controller/leader upgrade-ziu
 
-if ! wait_cmd_success 10 60 "ziu_status \"ziu is in progress - stage\/done = 0\/None\"" ; then
+if ! wait_cmd_success 10 60 "ziu_status_for_pattern \"ziu is in progress - stage\/done = 0\/None\"" ; then
     echo "ERROR: ziu have not started"
     exit 1
 fi
 
+echo "INFO: required units are in maintenance state. Update image tags...  $(date)"
+
 juju config tf-analytics image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
-(( $analyticsdb_enabled )) && juju config tf-analyticsdb image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+if [[ ${LEGACY_ANALYTICS_ENABLE,,} == 'true' ]]; then
+    juju config tf-analyticsdb image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+fi
 juju config tf-agent image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 juju config tf-openstack image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 juju config tf-controller image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 
-if ! wait_cmd_success 20 540 "ziu_status \"ziu is in progress - stage\/done = 5\/5\"" ; then
+echo "INFO: wait for control plane done  $(date)"
+
+if ! wait_cmd_success 20 540 "ziu_status_for_pattern \"ziu is in progress - stage\/done = 5\/5\"" ; then
     echo "ERROR: ziu've got an error before stage 5"
     exit 1
 fi
 # wait a bit when all agents consume stage 5
 sleep 60
 
+echo "INFO: Control plane is done. Run upgrade for agents...  $(date)"
+
 for agent in $(juju status | grep -o "tf-agent/[0-9]*"); do
     juju run-action --wait $agent upgrade
 done
 
-if ! wait_cmd_success 20 40 "juju status | grep -q \"ziu\"" ; then
+if ! wait_cmd_success 20 40 "juju_status_absent_pattern ziu" ; then
     echo "ERROR: ziu haven't finished"
     exit 1
 fi
 
+echo "INFO: agents are ready. Sync time and wait for active state  $(date)"
+
 sync_time
 
-if ! wait_cmd_success 60 40 "juju status | grep -q \"waiting\|blocked\|maintenance\|unknown\"" ; then
+if ! wait_cmd_success 60 40 "juju_status_absent_pattern \"waiting\|blocked\|maintenance\|unknown\"" ; then
     echo "ERROR: tf is not active after ziu"
     exit 1
 fi
+
+echo "INFO: ZIU is done  $(date)"
