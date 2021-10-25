dc754279be0e2355e277b75d292a5495393baf48
Alexey <alexey.morlang@gmail.com>
2021-10-25 21:10:42 +0300

    ubi8 build fix
    partial-jira-bugs: CEM-24319
    Change-Id: Id3d6bed8e3aaef3813451acc07ec504a4afcc385

--- build-containers.sh
+++ build-containers.sh
@@ -4,10 +4,17 @@ set -o pipefail
 CONTRAIL_REGISTRY=${CONTRAIL_REGISTRY:-"localhost:5000"}
 CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"dev"}
 
+LINUX_ID=$(awk -F"=" '/^ID=/{print $2}' /etc/os-release | tr -d '"')
+LINUX_VER_ID=$(awk -F"=" '/^VERSION_ID=/{print $2}' /etc/os-release | tr -d '"')
+
 scriptdir=$(realpath $(dirname "$0"))
 
 build_opts="--build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8"
 build_opts+=" --network host --no-cache --tag ${CONTRAIL_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG} $scriptdir"
+if [[ "$LINUX_ID" == 'rhel' && "${LINUX_VER_ID//.[0-9]*/}" == '8' ]] ; then
+    # podman case
+    build_opts+=' -v /etc/resolv.conf:/etc/resolv.conf:ro'
+fi
 
 sudo docker build $build_opts
 if [[ -n "$CONTRAIL_REGISTRY" ]]; then
