1d468490a40667b8eb78937d87a55c7db248883a
Alexey <alexey.morlang@gmail.com>
2022-02-08 16:49:14 +0300

    fixes for ubi8
    releate-jira-bug: CEM-25783
    Change-Id: I62adbcddaff34e74bd3be3f45e994974227399db

--- build-containers.sh
+++ build-containers.sh
@@ -11,10 +11,22 @@ scriptdir=$(realpath $(dirname "$0"))
 
 build_opts="--build-arg LC_ALL=en_US.UTF-8 --build-arg LANG=en_US.UTF-8 --build-arg LANGUAGE=en_US.UTF-8"
 build_opts+=" --network host --no-cache --tag ${CONTRAIL_REGISTRY}/tf-deployment-test:${CONTRAIL_CONTAINER_TAG} $scriptdir"
+
 if [[ "$LINUX_ID" == 'rhel' && "${LINUX_VER_ID//.[0-9]*/}" == '8' ]] ; then
-    # podman case
-    build_opts+=' --format docker'
-    build_opts+=' -v /etc/resolv.conf:/etc/resolv.conf:ro'
+  # podman case
+  build_opts+=' --format docker'
+  build_opts+=' --cap-add=all --security-opt label=disable --security-opt seccomp=unconfined'
+  build_opts+=' -v /etc/resolv.conf:/etc/resolv.conf:ro'
+  # to make posible use subscription inside container run from container in podman
+  if [ -e /run/secrets/etc-pki-entitlement ] ; then
+    build_opts+=' -v /run/secrets/etc-pki-entitlement:/run/secrets/etc-pki-entitlement:ro'
+  fi
+fi
+if [ -n "$LINUX_DISTR_VER" ] ; then
+  build_opts+=" --build-arg LINUX_DISTR_VER=${LINUX_DISTR_VER}"
+fi
+if [ -n "$LINUX_DISTR" ] ; then
+  build_opts+=" --build-arg LINUX_DISTR=${LINUX_DISTR}"
 fi
 
 sudo docker build $build_opts
